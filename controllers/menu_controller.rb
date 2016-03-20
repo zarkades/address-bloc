c7aa9daf12bfd084bbdb2eb7e7bf56266fcb5363
Zarkades <zarkades@gmail.com>
2016-03-20 11:24:27 -0700

    Assignment address-bloc-menu

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -16,7 +16,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - View Entry n"
+    puts "6 - Exit"
     print "Enter your selection: "
 
 # #3 Retrieve user input from the command line using gets.
@@ -40,6 +41,10 @@ class MenuController
       read_csv
       main_menu
     when 5
+      system "clear"
+      entry_n_submenu
+      main_menu
+    when 6
       puts "Good-bye!"
 # #8 Terminate the program using exit(0). 0 signals the program is exiting without an error
       exit(0)
@@ -48,6 +53,21 @@ class MenuController
         system "clear"
         puts "Sorry, that is not a valid input"
         main_menu
+      end
+    end
+
+    def entry_n_submenu
+      print "Entry number to view: "
+      selection = gets.chomp.to_i
+
+      if selection < @address_book.entries.count
+        puts @address_book.entries[selection]
+        puts "Press enter to return to the main menu"
+        gets.chomp
+        system "clear"
+      else
+        puts "#{selection} is not a valid input"
+        entry_n_submenu
       end
     end
 
