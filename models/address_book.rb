8c49a591ad265ed34580d3f7599ad46517107d29
Zarkades <zarkades@gmail.com>
2016-03-21 14:00:29 -0700

    Assignment address-bloc-searching

--- models/address_book.rb
+++ models/address_book.rb
@@ -54,4 +54,15 @@ class AddressBook
 
   return nil
  end
+
+ def iterative_search(name)
+   @entries.each do |entry|
+     if entry.name == name
+       return entry
+     end
+  end
+
+  return nil
+
+ end
 end
