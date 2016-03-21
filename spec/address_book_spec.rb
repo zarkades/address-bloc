16d6f1e5260d0a2fefe58e86c25851a247330e36
Zarkades <zarkades@gmail.com>
2016-03-21 12:13:28 -0700

    Assignment address-bloc-reading-csvs

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -90,4 +90,34 @@ end
     end
   end
 
+  describe "importing from entries_2.csv" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries_2.csv")
+
+      expect(book.entries.size).to eq 3
+    end
+
+    it "imports the 1st entry" do
+  book.import_from_csv("entries_2.csv")
+  # Check the firt entry
+  entry_one = book.entries[0]
+  check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+ end
+
+ it "imports the 2nd entry" do
+   book.import_from_csv("entries_2.csv")
+   # Check the second entry
+   entry_two = book.entries[1]
+   check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+ end
+
+ it "imports the 3rd entry" do
+   book.import_from_csv("entries_2.csv")
+   # Check the third entry
+   entry_three = book.entries[2]
+   check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+ end
+
+
+end
 end
